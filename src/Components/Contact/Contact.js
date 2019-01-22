ff9f3e2f65a511362dd3ce3594d74aefde43ae2a
Kalen Collins <kcollins1@allpointsllc.com>
2019-01-22 11:34:31 -0600

    added handler functions to contact and footer components

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -53,118 +53,154 @@ const contacts =[
 
 ]
 
-function Contact(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-          <Grid container justify="center" spacing={24}>
-        
-          <Grid item xs={12} >
-            <Typography color="secondary" variant="h4" className={classes.paragraph}>
-                Contact Us
-            </Typography>
-            <Typography variant="h3" gutterBottom>
-              <strong>
-                Reach out for a new project or just say hello
-              </strong>
-            </Typography>
+class Contact extends React.Component{
+  constructor(){
+    super()
 
-          </Grid>
+    this.state={
+      name: '',
+      email: '',
+      subject: '',
+      body: ''
+    }
+  }
+
+  handleChange = e =>{
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = e =>{
+    e.preventDefault();
+    console.log("fired")
+   const {name, email, subject, body} = this.state
+    //check if all fields are populated
+
+    //submit to endpoint
+
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+            <Grid container justify="center" spacing={24}>
+          
+            <Grid item xs={12} >
+              <Typography color="secondary" variant="h4" className={classes.paragraph}>
+                  Contact Us
+              </Typography>
+              <Typography variant="h3" gutterBottom>
+                <strong>
+                  Reach out for a new project or just say hello
+                </strong>
+              </Typography>
+  
+            </Grid>
+                      
+            <Grid item sm={6} >
+            <Paper className={classes.form} >
+            <form onSubmit={this.handleSubmit}>
+          
+            <TextField
+              InputLabelProps={{className:classes.textField}}
+              variant="outlined"
+              name="name"
+              label="Name"
+              fullWidth
+              margin="normal"
+              color="secondary"
+              onChange={this.handleChange}
+  
+              />
+               <TextField
+              InputLabelProps={{className:classes.textField}}
+              variant="outlined"
+              name="email"
+              label="Email"
+              fullWidth
+              margin="normal"
+              color="secondary"
+              onChange={this.handleChange}
+              />
+               <TextField            
+              InputLabelProps={{className:classes.textField}}
+              variant="outlined"
+              name="subject"
+              label="Subject"
+              fullWidth
+              margin="normal"
+              color="secondary"
+              onChange={this.handleChange}
+              />
+  
+               <TextField
+                  InputLabelProps={{className:classes.textField}}
+                  id="outlined-multiline-flexible"
+                  name="body"
+                  label="Body"
+                  multiline
+                  rows="10"
+                  rowsMax="40"
+                  fullWidth
+                  color="secondary"
+                  onChange={this.handleChange}
+                  margin="normal"
+                  variant="outlined"
+              />
+              <Button type="submit"> Submit</Button>
+            </form>
+            </Paper>
+          
+    
+            </Grid>
+  
+            <Grid item sm={4} >
+  
+              <Grid item >
+                <Typography color="secondary" variant="h4" >
+                    Contact Info
+                  </Typography>
+              </Grid>
+              
+  
+               <Grid item >
+                  {contacts.map(contact =>
+                  <Fragment>
+                    <Typography variant="title">
+                      <strong>
+                        {contact.name}
+                      </strong>
+                    </Typography>
+                    <Typography>
+                      {contact.phone}
+                    </Typography>
+  
+                    <Typography>
+                      {contact.addy.al1}, {contact.addy.al2}
+                    </Typography>
+  
+                    <Typography>
+                      {contact.addy.city},{contact.addy.state},{contact.addy.zip}
+                    </Typography>
+                      <hr class="hr" />
+                  </Fragment>
                     
-          <Grid item sm={6} >
-          <Paper className={classes.form}>
+                  )}
+              </Grid>
+              
+  
+            </Grid>
             
-          <TextField
-            InputLabelProps={{className:classes.textField}}
-            variant="outlined"
-            label="Name"
-            fullWidth
-            margin="normal"
-            color="secondary"
-
-            />
-             <TextField
-            InputLabelProps={{className:classes.textField}}
-            variant="outlined"
-            label="Email"
-            fullWidth
-            margin="normal"
-            color="secondary"
-
-            />
-             <TextField            
-            InputLabelProps={{className:classes.textField}}
-            variant="outlined"
-            label="Subject"
-            fullWidth
-            margin="normal"
-            color="secondary"
-
-
-            />
-
-             <TextField
-                InputLabelProps={{className:classes.textField}}
-                id="outlined-multiline-flexible"
-                label="Body"
-                multiline
-                rows="10"
-                rowsMax="40"
-                fullWidth
-                color="secondary"
-                //value={this.state.multiline}
-                //onChange={this.handleChange('multiline')}
-                margin="normal"
-                variant="outlined"
-            />
-
-          </Paper>
-        
   
           </Grid>
+  
+      </div>
+    );
 
-          <Grid item sm={4} >
-
-            <Grid item >
-              <Typography color="secondary" variant="h4" >
-                  Contact Info
-                </Typography>
-            </Grid>
-            
-
-             <Grid item >
-                {contacts.map(contact =>
-                <Fragment>
-                  <Typography variant="title">
-                    <strong>
-                      {contact.name}
-                    </strong>
-                  </Typography>
-                  <Typography>
-                    {contact.phone}
-                  </Typography>
-
-                  <Typography>
-                    {contact.addy.al1}, {contact.addy.al2}
-                  </Typography>
-
-                  <Typography>
-                    {contact.addy.city},{contact.addy.state},{contact.addy.zip}
-                  </Typography>
-                    <hr class="hr" />
-                </Fragment>
-                  
-                )}
-            </Grid>
-            
-
-          </Grid>
-          
-
-        </Grid>
-
-    </div>
-  );
+  }
+ 
 }
 
 Contact.propTypes = {
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,48 +5,75 @@ import { Grid, Typography, Button, TextField } from '@material-ui/core';
 
 import styles from '../Footer/styles'
 
-function Footer(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
+class Footer extends React.Component{
+  constructor(){
+    super()
+    this.state ={
+      email:''
+    }
+  }
+
+  handleChange = e =>{
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = e =>{
+    e.preventDefault();
+   const {email} = this.state
+   console.log(email)
+    //check if all fields are populated
+
+    //submit to endpoint
+
+  }
+  render (){
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+          <Grid container justify="center" spacing={12}>
+          
+            <Grid item xs={12} md={6}>
+              <Typography variant="subtitle" gutterBottom className={classes.paragraph}>
+             All Points Logo and terms and conditions. Proin eget tortor risus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Quaerat voluptas autem necessitatibus vitae aut.
+              </Typography>
   
-       
-        <Grid container justify="center" spacing={12}>
-        
-          <Grid item xs={12} md={6}>
-            <Typography variant="subtitle" gutterBottom className={classes.paragraph}>
-           All Points Logo and terms and conditions. Proin eget tortor risus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Quaerat voluptas autem necessitatibus vitae aut.
-            </Typography>
+            </Grid>
+                      
+            <Grid item xs={12} md={6}>
+              <Typography color="secondary" variant="subtitle" gutterBottom>
+              Get Notified
+              </Typography>
+              <Typography variant="subtitle" gutterBottom className={classes.paragraph}>
+              Want to stay updated with all things All Points? Enter your email  below to sign up for our weekly newsletter. We promise not to spam you.
+              </Typography>
 
+              <form onSubmit={this.handleSubmit}>
+              <TextField
+              name="email"
+              variant="outlined"
+              label="Email"
+              onChange={this.handleChange}
+              />
+              <Button variant="contained" size="large" type="submit">Subscribe</Button>
+              </form>
+            </Grid>
+  
+  
+            <Grid item xs={12} md={12}>
+              <Typography variant="subtitle" >
+              © Copyright All Points, LLC 2018 | <Button>User Portal</Button>
+              </Typography>
+  
+            </Grid>
+  
           </Grid>
-                    
-          <Grid item xs={12} md={6}>
-            <Typography color="secondary" variant="subtitle" gutterBottom>
-            Get Notified
-            </Typography>
-            <Typography variant="subtitle" gutterBottom className={classes.paragraph}>
-            Want to stay updated with all things All Points? Enter your email  below to sign up for our weekly newsletter. We promise not to spam you.
-            </Typography>
-
-            <TextField
-            variant="outlined"
-            label="Email"
-            />
-            <Button variant="contained" size="large">Subscribe</Button>
-          </Grid>
+      </div>
+    );
 
 
-          <Grid item xs={12} md={12}>
-            <Typography variant="subtitle" >
-            © Copyright All Points, LLC 2018 | <Button>User Portal</Button>
-            </Typography>
-
-          </Grid>
-
-        </Grid>
-
-    </div>
-  );
+  }
+ 
 }
 
 Footer.propTypes = {
